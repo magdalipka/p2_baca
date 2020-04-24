c8a58021b1d71b89396f9f54781c9a5f03347e2d
Magda Lipka <trzy1415926535@gmail.com>
2020-04-24 15:06:47 +0200

    working sums

--- Podprogramy/m.cpp
+++ Podprogramy/m.cpp
@@ -1,3 +1,4 @@
+//Magdalena Lipka//
 #include <iostream>
 #include <string>
 #include <cstdarg>
@@ -25,7 +26,7 @@ string add1 (string el1, string el2, string &wynik, int &carryover, int iterator
 
 		int sum = ( el1[iterator] - 48 ) + ( el2[iterator] - 48 ) + carryover;
 
-		wynik += (char)  (sum%10+48);
+		wynik += (char)  ( sum%10 + 48 );
 		carryover = sum/10;
 		
 		return add1 ( el1, el2, wynik, carryover, iterator+1, condition );
@@ -41,7 +42,7 @@ string add2 (string elem, string &wynik, int &carryover, int iterator, int condi
 
 		int sum = ( elem[iterator] - 48 ) + carryover;
 
-		wynik += (char) sum%10;
+		wynik += (char) ( sum%10 + 48 );
 		carryover = sum/10;
 		
 		return add2 ( elem, wynik, carryover, iterator+1, condition );
@@ -75,7 +76,7 @@ string Add (string el1, string el2) {
 	add1 ( el1, el2, des, carryover, 0, len1 );
 	add2 ( el2, des, carryover, len1, len2 );
 
-	if ( carryover ) des += (char) carryover;
+	if ( carryover ) des += (char) (carryover+48);
 
 	des = reverse ( des );
 
@@ -116,7 +117,7 @@ string diff1 (string el1, string el2, string &wynik, int &carryover, int iterato
 		}
 		else carryover = 0;
 
-		wynik += (char)  (diff+48);
+		wynik += (char) ( diff + 48 );
 		
 		return diff1 ( el1, el2, wynik, carryover, iterator+1, condition );
 	}
@@ -133,7 +134,7 @@ string diff2 (string elem, string &wynik, int &carryover, int iterator, int cond
 		}
 		else carryover = 0;
 
-		wynik += (char)  (diff+48);
+		wynik += (char) ( diff + 48 );
 		
 		return diff2 ( elem, wynik, carryover, iterator+1, condition );
 	}
@@ -158,9 +159,9 @@ string Diff (string el1, string el2) {
 	int carryover = 0;
 
 	diff1 ( el1, el2, des, carryover, 0, len2 );
-	diff2 ( el1, des, carryover, len1, len1 );
+	diff2 ( el1, des, carryover, len2, len1 );
 
-	if ( carryover ) des += (char) carryover;
+	if ( carryover ) des += (char) ( carryover + 48 );
 
 	des = reverse ( des );
 
@@ -214,6 +215,7 @@ string AddMany (const string* argv, string &wynik, int iterator, int condition) 
 			}
 		}
 		if ( wynik[0] == '-' ) {
+			wynik.erase(0, 1);
 			wynik = deletezeros(wynik);
 			wynik = '-' + wynik;
 		}
@@ -234,20 +236,18 @@ string Sum (int argc, const string* argv) {
 string AddVa(va_list lista, string &wynik, int iterator, int condition) {
 	if ( iterator < condition ) {
 
-		if ( wynik[0] != '-' && va_arg(lista, string)[0] != '-' )  {
-			string copy = va_arg(lista, string);
+		string copy = va_arg(lista, char*);
+		if ( wynik[0] != '-' && copy[0] != '-' )  {
 			if ( copy[0] == '+' ) copy.erase(0, 1);
 			wynik = Add(wynik, copy);
 		}
-		else if (wynik[0] == '-' && va_arg(lista, string)[0] == '-') {
+		else if (wynik[0] == '-' && copy[0] == '-') {
 			wynik.erase(0, 1);
-			string copy = va_arg(lista, string);
 			copy.erase(0, 1);
 			wynik = '-' + Add(wynik, copy);
 
 		}
 		else if ( wynik[0]=='-' ) {
-			string copy = va_arg(lista, string);
 			if ( copy[0] == '+' ) copy.erase(0, 1);
 			wynik.erase(0, 1);
 			if ( smaller ( wynik, copy ) ) {
@@ -259,7 +259,6 @@ string AddVa(va_list lista, string &wynik, int iterator, int condition) {
 		}
 		else {
 			//arcgv[0]=='-'//
-			string copy = va_arg(lista, string);
 			copy.erase(0, 1);
 			if ( smaller ( wynik, copy ) ) {
 				wynik = '-' + Diff(wynik, copy);
@@ -269,13 +268,13 @@ string AddVa(va_list lista, string &wynik, int iterator, int condition) {
 			}
 		}
 		if ( wynik[0] == '-' ) {
+			wynik.erase(0, 1);
 			wynik = deletezeros(wynik);
 			wynik = '-' + wynik;
 		}
 		else wynik = deletezeros(wynik);
 		if ( wynik == "" || wynik == "-0" || wynik == "-" ) wynik = "0";
 		return AddVa(lista, wynik, iterator+1, condition);
-
 	} 
 	else return wynik;
 }
@@ -283,15 +282,35 @@ string AddVa(va_list lista, string &wynik, int iterator, int condition) {
 string Sum ( int argc, ... ) {
 	string wynik = "0";
 	va_list lista;
-
 	va_start(lista, argc);
-
-
 	return AddVa(lista, wynik, 0, argc );
+}
+
+void Sum ( string* wynik, int liczba, const string* napis ) {
+	*wynik = Sum ( liczba, napis );
+}
+void Sum ( string* wynik, int liczba, ... ) {
+	string des = "0";
+	va_list lista;
+	va_start(lista, liczba);
+	*wynik = AddVa (lista, des, 0, liczba);
+}
+void Sum ( string &wynik, int liczba, const string* napis ) {
+	wynik = Sum ( liczba, napis );
+}
+void Sum ( string &wynik, int liczba, ... ) {
+	string des = "0";
+	va_list lista;
+	va_start(lista, liczba);
+	wynik = AddVa (lista, des, 0, liczba);
 }
 
 int main () {
 
-	cout << Diff("254", "255");
+	string napis[] = {"+000","-0"};
+
+	cout << Sum(2, napis);
+
+  return 0;
 
 }
